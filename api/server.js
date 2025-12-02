d3521a9cd76d3de8d252a6f5b4f910480ddfe5c4
wolfix1234 <130220073+wolfixor@users.noreply.github.com>
2025-12-03 02:40:47 +0330

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-  res.send("TEST1 Service is working!");
+  res.send("TEST Service is working!");
 });
 app.listen(8000, () => console.log("API service listening on 8000"));
