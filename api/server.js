9b24ae8d7f337cc7d8915850612c5faf0aca2999
wolfix1234 <130220073+wolfixor@users.noreply.github.com>
2025-12-03 02:07:40 +0330

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-  res.send("API Service is working!");
+  res.send("MAMAD Service is working!");
 });
 app.listen(8000, () => console.log("API service listening on 8000"));
